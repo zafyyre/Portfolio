9750029b355a0e04a803e43fc8ee8c4cd46e07d9
zafyyre <zaaf17@hotmail.com>
2024-05-18 13:07:38 -0700

    fixed how the load works for gamesList.html

--- src/eventListeners.js
+++ src/eventListeners.js
@@ -27,7 +27,7 @@ function onButtonHover(hoveredObject) {
       const buttonData = buttonsData[buttonIndex];
       alert(`Button ${buttonData.label} clicked!`);
       if (buttonData.label === 'Games') {
-        window.location.href = 'src/games/gamesList.html';
+        window.location.href = '/src/games/gamesList.html';
       }
     }
   }
