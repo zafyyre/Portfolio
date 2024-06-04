88092396aa6b0f07ce30623e9696bb12534bee4e
zafyyre <zaaf17@hotmail.com>
2024-06-04 14:25:38 -0700

    Added default behaviour to onEnd and used passive event listeners for touchstart

--- src/utils/eventListeners.js
+++ src/utils/eventListeners.js
@@ -92,11 +92,7 @@ function getClientCoordinates(event) {
  * @param {Event} event - The event object (mouse or touch).
  */
 function onStart(event) {
-
-  if (event.type === 'touchstart') {
-    event.preventDefault(); // Prevent default behavior (e.g., scrolling) for touch events
-  }
-
+  
   const { x, y } = getClientCoordinates(event);
   startX = x;
   startY = y;
@@ -108,7 +104,11 @@ function onStart(event) {
  * @param {Event} event - The event object (mouse or touch).
  */
 function onEnd(event) {
-  
+
+  if (event.type === 'touchend') {
+    event.preventDefault(); // Prevent default behavior for touchend events
+  }
+
   if (isMouseDown) {
     const { x, y } = getClientCoordinates(event);
     const dx = x - startX;
